c7d6e4927db8822999b41d4dcb13492c5af25c1f
John <jmorgan@basecampcodingacademy.org>
2019-01-30 18:20:10 -0600

    Added the EmojiPart Compnent as a part for Toolbar.

--- app/src/components/ToolBar.js
+++ app/src/components/ToolBar.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import '../css/style.css';
-
+import React from "react";
+import "../css/toolbar.css";
+import EmojiPart from "./EmojiPart";
 
 export class ToolBar extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { overFlowing: false };
+        this.state = {};
     }
 
     render() {
         return (
-        <div className="toolbar">
-            <button className="leftBtn">L</button>
-            <button className="rightBtn">R</button>
-        </div>
+            <div className="toolbar">
+                <button className="leftBtn">L</button>
+                <div className="parts">
+                    {this.props.emojis.map(emoji => (
+                        <EmojiPart />
+                    ))}
+                </div>
+                <button className="rightBtn">R</button>
+            </div>
         );
     }
 }
-
