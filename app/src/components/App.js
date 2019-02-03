c9b40c301e0e6b229dae8194c393ef77386e7265
John <jmorgan@basecampcodingacademy.org>
2019-02-03 10:22:53 -0600

    Added an Inventory to be passed down to Toolbar.

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
-import {ToolBar} from './ToolBar';
+import React, { Component } from "react";
+import { ToolBar } from "./ToolBar";
 
+const inv = {
+    heads: [
+        {id: 1, src: "/images/blank_face.png", alt: "" },
+        {id: 2, src: "/images/blank_face.png", alt: "" },
+        {id: 3, src: "/images/blank_face.png", alt: "" }
+    ]
+};
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <ToolBar />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <ToolBar emojis={inv} />
+            </div>
+        );
+    }
 }
 
 export default App;
