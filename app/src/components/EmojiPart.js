d859cb35885201f89b4b5424063f8e951f3fec33
John <jmorgan@basecampcodingacademy.org>
2019-02-03 10:25:07 -0600

    moved the toggle selected into Toolbar and passed down as a prop.

---
+++ app/src/components/EmojiPart.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "../css/emoji-part.css";
+
+export class EmojiPart extends Component {
+    constructor(props) {
+        super(props);
+        
+    }
+
+    render() {
+        var className;
+        if (this.props.selected == this.props.id) {
+            className = "emoji-part active";
+        } else {
+            className = "emoji-part";
+        }
+
+        return (
+            <div className={className} onClick={this.props.onClick}>
+                <p>hey</p>
+            </div>
+        );
+    }
+
+}
+
+export default EmojiPart;
