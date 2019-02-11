bbb8ce4f83a26bb249a368a6f0d52a141c1fb34d
John <jmorgan@basecampcodingacademy.org>
2019-02-11 16:00:45 -0600

    Initialized Canvas to begin building the emoji inside of it.

---
+++ app/src/components/Canvas.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "../css/canvas.css";
+
+export class Canvas extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div id="canvas">
+                
+            </div>
+        )
+    }
+}
