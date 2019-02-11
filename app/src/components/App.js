8cfff8abcf8865f10c4c098dc3a5d7425930e583
John <jmorgan@basecampcodingacademy.org>
2019-02-11 16:00:12 -0600

    Moved Toolbar state and toggleSelected into this component pass information down into both Toolbar and Canvas.

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,20 +1,32 @@
 import React, { Component } from "react";
+import { Canvas } from "./Canvas";
 import { ToolBar } from "./ToolBar";
 
 const inv = {
     heads: [
-        {id: 1, src: "/images/blank_face.png", alt: "" },
-        {id: 2, src: "/images/blank_face.png", alt: "" },
-        {id: 3, src: "/images/blank_face.png", alt: "" }
+        { id: 1, src: "/images/blank_face.png", alt: "" },
+        { id: 2, src: "/images/blank_face.png", alt: "" },
+        { id: 3, src: "/images/blank_face.png", alt: "" }
     ]
 };
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {tab: "head", selected: 0};
+        this.toggleSelected = this.toggleSelected.bind(this);
+    }
+
     render() {
         return (
             <div className="App">
-                <ToolBar emojis={inv} />
+                <Canvas />
+                <ToolBar emojis={inv} toggleSelected={this.toggleSelected} selected={this.state.selected}/>
             </div>
         );
+    }
+
+    toggleSelected(id) {
+        this.setState({selected: id});
     }
 }
 
