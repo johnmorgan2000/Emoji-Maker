48d1c1c2a9dcbdc900a51087bbc8d6723d7070f1
John <jmorgan@basecampcodingacademy.org>
2019-02-14 15:54:53 -0600

    Created a key and made toggleSelect take in a src.

--- app/src/components/ToolBar.js
+++ app/src/components/ToolBar.js
@@ -11,10 +11,13 @@ export class ToolBar extends React.Component {
         return (
             <div className="toolbar">
                 <div className="parts">
-                    {this.props.emojis.heads.map(emoji => (
-                        <EmojiPart id={emoji.id} 
-                        selected={this.props.selected} 
-                        onClick={()=> this.props.toggleSelected(emoji.id)}/>))
+                    {this.props.emojis.map((emoji, key) => (
+                        <EmojiPart 
+                        key={key}
+                        id={emoji.id} 
+                        selected={this.props.selected}
+                        src={emoji.src}
+                        onClick={()=> this.props.toggleSelected(emoji.id,emoji.src, this.props.part)}/>))
                     }
                 </div>
             </div>
