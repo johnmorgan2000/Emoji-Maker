7233ab713af5abfdfde295c2fe688bd64b3820a7
John <jmorgan@basecampcodingacademy.org>
2019-02-14 15:53:54 -0600

    And the eyes and mouth images.

--- app/src/components/Canvas.js
+++ app/src/components/Canvas.js
@@ -9,7 +9,9 @@ export class Canvas extends React.Component {
     render(){
         return (
             <div id="canvas">
-                
+                <img id="head" src={this.props.headSrc} />
+                <img id="eyes" src={this.props.eyesSrc} />
+                <img id="mouth" src={this.props.mouthSrc} />
             </div>
         )
     }
